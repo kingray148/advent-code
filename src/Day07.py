39b492601bae10f310f6f95ccdbc90b00dc1f862
TK <21750101@qq.com>
2022-01-08 03:52:17 +0800

    upload data for code

--- src/Day07.py
+++ src/Day07.py
@@ -8,9 +8,21 @@
 """
 
 
-def func(name):
-    pass
+def leastFuel(file):
+    with open(file, 'r') as f:
+        crabposition = f.readlines()
+        crabposition = [int(x) for x in crabposition[0].split(',')]
+        #print(sum(crabposition))
+        #print(len(crabposition))
+    positions = set(crabposition)
+    fuelQty = []
+    for x in positions:
+        fuel = 0
+        for y in crabposition:
+            fuel += abs(x-y)
+        fuelQty.append(fuel)
+    return min(fuelQty)
 
 
 if __name__ == '__main__':
-    func(hello)
+    print(leastFuel('..//files//Day 7 data.txt'))
---
+++ src/Day08.py
@@ -0,0 +1,17 @@
+"""
+# -*- coding: utf-8 -*-
+@Created on 3:51 2022-01-08
+
+@Describe:
+https://adventofcode.com/2021/day/8
+
+@author: TK
+"""
+
+
+def func(name):
+    pass
+
+
+if __name__ == '__main__':
+    func(hello)
