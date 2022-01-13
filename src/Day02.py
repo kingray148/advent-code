7088787ab3672a40d8409d30a8b01e436dff4397
TK <21750101@qq.com>
2022-01-13 16:45:22 +0800

    upload data for code

--- src/Day02.py
+++ src/Day02.py
@@ -10,24 +10,43 @@ all data are stored in txt file: Day 2 data.txt
 
 @author: TK
 """
+def readData(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    return data
 
-def calPosition(file):
+def calPosition(data):
     horizon = 0
     depth = 0
-    with open(file, 'r') as f:
-        for x in f.readlines():
-            #print(x.split())
-            direction, step = x.split()
-            if direction == 'forward':
-                horizon += int(step)
-            elif direction == 'up':
-                depth -= int(step)
-            elif direction == 'down':
-                depth += int(step)
+    for x in data:
+        #print(x.split())
+        direction, step = x.split()
+        if direction == 'forward':
+            horizon += int(step)
+        elif direction == 'up':
+            depth -= int(step)
+        elif direction == 'down':
+            depth += int(step)
     return [horizon, depth]
 
+def posWithAim(data):
+    horizon = 0
+    depth = 0
+    aim = 0
+    for x in data:
+        direction, step = x.split()
+        if direction == 'down':
+            aim += int(step)
+        elif direction == 'up':
+            aim -= int(step)
+        elif direction == 'forward':
+            horizon += int(step)
+            depth += int(step) * aim
+    return horizon*depth
+
 if __name__ == '__main__':
-    x = calPosition('..\\files\\Day 2 data.txt')
+    x = calPosition(readData('..\\files\\Day 2 data.txt'))
     #horizon = x[0]
     #depth = x[1]
     print('Horizon is: ' + str(x[0]) + ', Depth is: ' + str(x[1]) + ', Position is: ' + str(x[0] * x[1]))
+    print(posWithAim(readData('..\\files\\Day 2 data.txt')))
--- src/Day03.py
+++ src/Day03.py
@@ -8,9 +8,10 @@ https://adventofcode.com/2021/day/3
 
 @author: TK
 """
-import urllib.request
+#import urllib.request
 
 #from urllib.request import urlopen
+import numpy as np
 
 def powerConsumption(file):
     with open(file, 'r') as f:
@@ -34,8 +35,8 @@ def powerConsumption(file):
         else:
             gamma[i] = '0'
             epsilon[i] = '1'
-    #print(gamma)
-    #print(epsilon)
+    print(gamma)
+    print(epsilon)
     gamma = int(''.join(gamma),2)
     epsilon = int(''.join(epsilon), 2)
     #print(count)
@@ -43,6 +44,50 @@ def powerConsumption(file):
     #print(gamma)
     #print(epsilon)
     return (gamma*epsilon)
+
+def lifeSupportRating(file):
+    with open(file, 'r') as f:
+        input = f.readlines()
+    columns = len(input[0].strip())
+    rows = len(input)
+    input = list(''.join(''.join(input).split('\n')))
+    input = np.reshape(input, (rows,columns))   #将数据转换为二维数组以便计算
+    #print(input)
+    #print(rows)
+    #print(columns)
+    oxygenRating = input.copy()     #复制input数据进行下一步计算
+    co2Rating = input.copy()
+    for i in range(0, columns):
+        if len(oxygenRating[oxygenRating[:,i]=='1']) >= len(oxygenRating[oxygenRating[:,i]=='0']):  #找出数组某一列钟哪个元素数量较多，保留较多数元素的行
+            oxygenRating = oxygenRating[oxygenRating[:,i]=='1']
+        else:
+            oxygenRating = oxygenRating[oxygenRating[:,i]=='0']
+        #oxygenRating = input[input[:,i]==1] if len(input[input[:,i]==1]) >= len(input[input[:,i]==0]) else input[input[:,i]==0]
+        if len(oxygenRating) == 1:  #如果剩余数据只有一行，则停止循环返回结果
+            break
+    for i in range(0, columns):
+        if len(co2Rating[co2Rating[:,i]=='1']) >= len(co2Rating[co2Rating[:,i]=='0']) and len(co2Rating[co2Rating[:,i]=='0']) != 0: #找出数组某一列钟元素数量较少，如果较少元素个数不为零则保留较少元素所在行，否则保留个数较多的元素所在行
+            co2Rating = co2Rating[co2Rating[:,i]=='0']
+        elif len(co2Rating[co2Rating[:,i]=='1']) !=0 :  #与上同理，较少元素行数不为零时取较少元素所在行，否则保留较多元素的行
+            co2Rating = co2Rating[co2Rating[:,i]=='1']
+        else:
+            co2Rating = co2Rating[co2Rating[:,i]=='0']
+        #co2Rating = input[input[:,i]==1] if len(input[input[:,i]==1]) < len(input[input[:,i]==0]) else input[input[:,i]==0]
+        #print(i)
+        #print(co2Rating)
+        #print('.............')
+        if len(co2Rating) == 1: #只剩一行时，停止循环
+            break
+    oxygenRating = oxygenRating.tolist()    #将np.array数组转换为list
+    oxygenRating = oxygenRating[0]          #由于转换后数据仍然是二维，需要先降维再转换为string
+    #print(oxygenRating)
+    oxygenRating = int(''.join(oxygenRating),2) #将元素组合为string，然后从二进制转换为十进制数据
+    co2Rating = co2Rating.tolist()
+    co2Rating = co2Rating[0]
+    co2Rating = int(''.join(co2Rating),2)
+    #print(oxygenRating)
+    #print(co2Rating)
+    return oxygenRating*co2Rating
 
 #def webop(url):
 #    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
@@ -52,6 +97,7 @@ def powerConsumption(file):
 
 
 if __name__ == '__main__':
-    print(powerConsumption('..\\files\\Day 3 data.txt'))
+    #print(powerConsumption('..\\files\\Day 3 data.txt'))
     #webop("https://adventofcode.com/2021/day/3/input")
     #webop('https://www.google.com')
+    print(lifeSupportRating('..//files//Day 3 data.txt'))
