bec572adae5ed05a5a2a887470a5c79fdacc4fb2
TK <21750101@qq.com>
2022-01-20 03:10:57 +0800

    upload data for code

--- src/Day09.py
+++ src/Day09.py
@@ -35,5 +35,48 @@ def riskSum(file):
                 riskSum += 1 + input[i,j]
     return riskSum
 
+def top3Basin(file):
+    with open(file, 'r') as f:
+        input = f.readlines()
+    columns = len(input[0].strip())
+    input = ''.join(input).split('\n')
+    input = [int(x) for x in list(''.join(input))]
+    rows = int(len(input)/columns)
+    #print(columns)
+    #print(rows)
+    input = np.reshape(input, (rows,columns))
+    #print(input)
+    #print(input.shape)
+    riskSum = 0
+    maxnum = np.max(input)
+    input = np.insert(input, 0, maxnum, axis=0)
+    input = np.insert(input, rows+1, maxnum, axis=0)
+    input = np.insert(input, 0, maxnum, axis=1)
+    input = np.insert(input, columns+1, maxnum, axis=1)
+    #print(input)
+    #print(input.shape)
+    basinPoints = []
+    result = []
+    for i in range(1, rows+1):      #先找出全部数组中的地点
+        for j in range(1, columns+1):
+            if input[i,j] < min(input[i-1,j], input[i,j-1], input[i+1,j], input[i,j+1]):
+                basinPoints.append([i,j])
+    for x in basinPoints:
+        i, j = x
+        basinArea = []
+        stack = []
+        basinArea.append([i,j])
+        stack += [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]
+        while stack:
+            a,b = stack.pop()
+            if input[a,b] != 9 and [a,b] not in basinArea:
+                basinArea.append([a,b])
+                stack += [[a-1,b], [a+1,b], [a,b-1], [a,b+1]]
+        result.append(len(basinArea))
+    result.sort(reverse=True)
+    #print(result)
+    return(result[0]*result[1]*result[2])
+
 if __name__ == '__main__':
     print(riskSum('..//files//Day 9 data.txt'))
+    print(top3Basin('..//files//Day 9 data.txt'))
--- src/Day10.py
+++ src/Day10.py
@@ -29,5 +29,36 @@ def syntaxErrorScore(file):
                 break
     return errorScore
 
+def middleScore(file):
+    bracketReverseDict = {'>':'<', ')':'(', ']':'[', '}':'{'}
+    scoreTable = {'(':'1', '[':'2', '{':'3', '<':'4'}
+    with open(file, 'r') as f:
+        input = f.readlines()
+    input = [list(x.strip()) for x in input]
+    #print(input)
+    completionScoreList = []
+    n = 1
+    for x in input:
+        stack = []
+        completionStr = []
+        flag = 1
+        for i in range(len(x)):
+            if x[i] in bracketReverseDict.values():
+                stack.append(x[i])
+            elif bracketReverseDict[x[i]] == stack[-1]:
+                stack.pop()
+            else:
+                flag = 0
+                break
+        if flag:
+            stack.reverse()
+            completionStr = [scoreTable[y] for y in stack]
+            #print(completionStr)
+            completionScoreList.append(int(''.join(completionStr),5))
+    completionScoreList.sort()
+    #print(completionScoreList)
+    return completionScoreList[int(len(completionScoreList)/2)]
+
 if __name__ == '__main__':
     print(syntaxErrorScore('..//files//Day 10 data.txt'))
+    print(middleScore('..//files//Day 10 data.txt'))
